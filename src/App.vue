7937143a1d036e031f1902785b6dbb2c3027b249
jalis <jalismahamud31@gmail.com>
2025-12-16 20:14:41 +0600

    Multiple v-model to one component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import { reactive, ref } from 'vue'
 import TestComponent from './components/TestComponent.vue';
 import PersonComponent from './components/PersonComponent.vue';
+import NameComponent from './components/NameComponent.vue';
+
 
 const name = ref('Jalis Mahamud')
 const email = ref('jon@gmail.com')
@@ -11,17 +13,31 @@ const person = reactive({
   email: 'jalis@mahamud.com',
   age: 25
 });
+
+const fname = ref('Jalis');
+const lname = ref('Mahamud');
 </script>
 
 <template>
-  <div>
+  <div class="text-2xl text-green-600">
+      Original Frist Name: {{ fname }} <br>
+      Original Last Name: {{ lname }}
+  </div>
+  <!-- <div>
     <p>Name: {{ person.name }}</p>
     <p>Email: {{ person.email }}</p>
     <p>Age: {{ person.age }}</p>
+  </div> -->
+
+  <div>
+    <i class="fas fa-solar-panel    ">Component</i>
+    <p>
+      <NameComponent v-model:fname="fname" v-model:lname="lname" />
+    </p>
   </div>
 
- <div>
+ <!-- <div>
    <PersonComponent :person="person" />
- </div>
+ </div> -->
  
 </template>
---
+++ src/components/NameComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+    const props = defineProps([
+        'fname' , 'lname'
+    ]);
+
+    const emit = defineEmits(['update:fname' , 'update:lname']);
+</script>
+
+<template>
+    <p>
+        <label for="">Frist Name</label>
+        <input type="text" :value="fname" @input="$emit('update:fname', $event.target.value)">
+    </p>
+    <p>
+        <label for="">Last Name</label>
+        <input type="text" :value="lname" @input="$emit('update:lname' , $event.target.value)">
+    </p>
+</template>
