f5e9acf6fa58e13b9a1b5d7488465a5207a38461
jalis <jalismahamud31@gmail.com>
2025-12-16 19:01:13 +0600

    test component with input feild

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["label", "modelValue"]);
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <div>
+    <label>{{ label }}</label><br />
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
